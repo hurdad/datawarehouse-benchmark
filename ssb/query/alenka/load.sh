e41ee1924f330663f2768c4094e61b9f3e921bc3
Alexander Hurd <hurdad@gmail.com>
2016-03-03 21:24:04 -0500

    add load scripts

---
+++ ssb/query/alenka/load.sh
@@ -0,0 +1,6 @@
+./alenka load_supplier.sql
+./alenka load_customer.sql
+./alenka load_part.sql
+./alenka load_supplier.sql
+./alenka load_date.sql
+./alenka -l 200 load_lineorder.sql
---
+++ ssb/query/alenka/load_bitmap.sh
@@ -0,0 +1,20 @@
+./alenka load_supplier.sql
+./alenka load_customer.sql
+./alenka load_part.sql
+./alenka load_supplier.sql
+./alenka load_date.sql
+./alenka -l 200 load_lineorder.sql
+
+./alenka index2.sql 
+./alenka index3.sql 
+./alenka index4.sql 
+./alenka index5.sql 
+./alenka index6.sql 
+./alenka index7.sql
+./alenka index8.sql  
+./alenka index9.sql 
+./alenka index10.sql 
+./alenka index11.sql 
+./alenka index12.sql 
+./alenka index13.sql 
+./alenka index14.sql 
